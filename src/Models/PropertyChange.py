a18406f73f1c98ed45db6a67040a9a39aace71a1
ivano <ivanomatrisciano@gmail.com>
2025-05-29 09:57:07 +0200

    Added analysis of changed properties

--- src/Models/PropertyChange.py
+++ src/Models/PropertyChange.py
@@ -1,10 +1,13 @@
 class PropertyChange:
     def __init__(self):
-        self.property_path: list = None
+        self.property_path: list[str] = None
         self.before_value = None
         self.after_value = None
 
-    def __init__(self, property_path: list, before_value, after_value):
+    def __init__(self, property_path: list[str], before_value, after_value):
         self.property_path = property_path
         self.before_value = before_value
         self.after_value = after_value
+
+    def property_path_str(self) -> str:
+        return str.join('.', self.property_path)
