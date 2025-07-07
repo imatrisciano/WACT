10b8a447c9c6e5d8ba6ad2fbdd4e97f686837e0e
Ivano Matrisciano <ivanomatrisciano@gmail.com>
2025-07-07 14:29:23 +0200

    Refactored keyboard_player into a class

--- src/InteractiveExample/Simulation/keyboard_player.py
+++ src/InteractiveExample/Simulation/keyboard_player.py
@@ -12,148 +12,151 @@ from ai2thor.platform import CloudRendering
 from src.InteractiveExample.Simulation.keyboard_player_constants import perform_constants_fixups
 from src.InteractiveExample.Simulation.keyboard_player_utils import *
 
-perform_constants_fixups()
+
+class KeyboardPlayer:
+    def __init__(self):
+        perform_constants_fixups()
+        self.controller: Controller = None
+
+    def _keyboard_play(self, top_down_frames, first_view_frames, is_rotate, rotate_per_frame):
+        self._show_frames()
+
+        step = 0
+        pickup = None
+        objectId = None
+
+        while True:
+            keystroke = cv2.waitKey(0)
+            step += 1
+
+            if keystroke == ord(actionList["FINISH"]):
+                self._stop_environment()
+                return
+
+            action, objectId, pickup = get_action_and_object(keystroke, self.controller, objectId, pickup)
+            if action is None:
+                continue # no valid action was selected, try again
+
+            self._execute_action(action, objectId)
+
+            if is_rotate:
+                self._rotate_third_view_camera(rotation_angle=rotate_per_frame)
+
+            # Show and get frames
+            first_view_frame, top_down_frame = self._show_frames()
+
+            top_down_frames.append(top_down_frame)
+            first_view_frames.append(first_view_frame)
 
 
-def keyboard_play(env, top_down_frames, first_view_frames, is_rotate, rotate_per_frame):
-    _show_frames(env)
+    def _stop_environment(self):
+        self.controller.stop()
+        cv2.destroyAllWindows()
+        print("action: STOP")
 
-    step = 0
-    pickup = None
-    objectId = None
+    def _rotate_third_view_camera(self, rotation_angle):
+        ## rotation third camera
+        last_event = self.controller.last_event
+        pose = compute_rotate_camera_pose(last_event.metadata["sceneBounds"]["center"],
+                                          last_event.metadata["thirdPartyCameras"][0], rotation_angle)
+        self.controller.step(
+            action="UpdateThirdPartyCamera",
+            **pose
+        )
 
-    while True:
-        keystroke = cv2.waitKey(0)
-        step += 1
+    def _execute_action(self, action, objectId):
+        action_has_target: bool = "Object" in action
+        if action_has_target:
+            self.controller.step(action=action, objectId=objectId)
+        else:
+            self.controller.step(action=action)
 
-        if keystroke == ord(actionList["FINISH"]):
-            _stop_environment(env)
-            return
+    def _show_frames(self):
+        """
+        Shows and returns both first view and top view frames from the scene
+        :param env: The ai2thor environment
+        :return: first_view_frame, top_down_frame
+        """
 
-        action, objectId, pickup = get_action_and_object(keystroke, env, objectId, pickup)
-        if action is None:
-            continue # no valid action was selected, try again
+        first_view_frame = self.controller.last_event.frame
+        cv2.imshow("first_view", cv2.cvtColor(first_view_frame, cv2.COLOR_RGB2BGR))
 
-        _execute_action(env, action, objectId)
+        # remove the ceiling
+        self.controller.step(action="ToggleMapView")
+        top_down_frame = self.controller.last_event.third_party_camera_frames[0]
+        cv2.imshow("top_view", cv2.cvtColor(top_down_frame, cv2.COLOR_RGB2BGR))
+        self.controller.step(action="ToggleMapView")
 
-        if is_rotate:
-            _rotate_third_view_camera(env, rotation_angle=rotate_per_frame)
-
-        # Show and get frames
-        first_view_frame, top_down_frame = _show_frames(env)
-
-        top_down_frames.append(top_down_frame)
-        first_view_frames.append(first_view_frame)
-
-
-def _stop_environment(env):
-    env.stop()
-    cv2.destroyAllWindows()
-    print("action: STOP")
-
-def _rotate_third_view_camera(env, rotation_angle):
-    ## rotation third camera
-    pose = compute_rotate_camera_pose(env.last_event.metadata["sceneBounds"]["center"],
-                                      env.last_event.metadata["thirdPartyCameras"][0], rotation_angle)
-    env.step(
-        action="UpdateThirdPartyCamera",
-        **pose
-    )
+        return first_view_frame, top_down_frame
 
 
-def _execute_action(env, action, objectId):
-    action_has_target: bool = "Object" in action
-    if action_has_target:
-        env.step(action=action, objectId=objectId)
-    else:
-        env.step(action=action)
+    def run(self, scene_name="FloorPlan205_physics", gridSize=0.25, rotateStepDegrees=15,
+             birds_eye_view=False, slope_degree=45, down_angle=65, use_procthor=False, procthor_scene_file="", procthor_scene_num=100,
+             is_rotate=True, rotate_per_frame=6, generate_video=False, generate_gif=False):
+        ## procthor room
+        if use_procthor:
+            with gzip.open(procthor_scene_file, "r") as f:
+                houses = [line for line in tqdm(f, total=10000, desc=f"Loading train")]
+            ## procthor train set's room
+            house = json.loads(houses[procthor_scene_num])
+        else:
+            ## select room, 1-30，201-230，301-330，401-430 are ithor's room
+            house = scene_name
 
+        self.controller = Controller(
+            agentMode="default",
+            visibilityDistance=5,
+            renderInstanceSegmentation=True,
+            scene=house,
+            # step sizes
+            gridSize=gridSize,
+            snapToGrid=False,
+            rotateStepDegrees=rotateStepDegrees,
+            # camera properties
+            width=1200,
+            height=800,
+            fieldOfView=90,
+            platform=CloudRendering,
+        )
 
-def _show_frames(env):
-    """
-    Shows and returns both first view and top view frames from the scene
-    :param env: The ai2thor environment
-    :return: first_view_frame, top_down_frame
-    """
+        ## add third view camera
+        event = self.controller.step(action="GetMapViewCameraProperties")
+        ## third camera's fov
+        third_fov = 60
 
-    first_view_frame = env.last_event.frame
-    cv2.imshow("first_view", cv2.cvtColor(first_view_frame, cv2.COLOR_RGB2BGR))
+        if not birds_eye_view:
+            ## top_view(slope)
+            pose = initialize_side_camera_pose(event.metadata["sceneBounds"], event.metadata["actionReturn"], third_fov,
+                                               slope_degree, down_angle)
+        else:
+            ## BEV
+            pose = event.metadata["actionReturn"]
+            is_rotate = False  ## assume that BEV do not need rotation
 
-    # remove the ceiling
-    env.step(action="ToggleMapView")
-    top_down_frame = env.last_event.third_party_camera_frames[0]
-    cv2.imshow("top_view", cv2.cvtColor(top_down_frame, cv2.COLOR_RGB2BGR))
-    env.step(action="ToggleMapView")
+        event = self.controller.step(
+            action="AddThirdPartyCamera",
+            skyboxColor="black",
+            fieldOfView=third_fov,
+            **pose
+        )
 
-    return first_view_frame, top_down_frame
+        ## collect frame
+        first_view_frames = []
+        third_view_frames = []
 
+        ## use keyboard control agent
+        self._keyboard_play(third_view_frames, first_view_frames, is_rotate, rotate_per_frame)
 
-def main(scene_name="FloorPlan205_physics", gridSize=0.25, rotateStepDegrees=15,
-         birds_eye_view=False, slope_degree=45, down_angle=65, use_procthor=False, procthor_scene_file="", procthor_scene_num=100,
-         is_rotate=True, rotate_per_frame=6, generate_video=False, generate_gif=False):
-    ## procthor room
-    if use_procthor:
-        with gzip.open(procthor_scene_file, "r") as f:
-            houses = [line for line in tqdm(f, total=10000, desc=f"Loading train")]
-        ## procthor train set's room
-        house = json.loads(houses[procthor_scene_num])
-    else:
-        ## select room, 1-30，201-230，301-330，401-430 are ithor's room
-        house = scene_name
+        if generate_video:
+            save_videos(scene_name, first_view_frames, third_view_frames)
 
-    controller = Controller(
-        agentMode="default",
-        visibilityDistance=5,
-        renderInstanceSegmentation=True,
-        scene=house,
-        # step sizes
-        gridSize=gridSize,
-        snapToGrid=False,
-        rotateStepDegrees=rotateStepDegrees,
-        # camera properties
-        width=1200,
-        height=800,
-        fieldOfView=90,
-        platform=CloudRendering,
-    )
-
-    ## add third view camera
-    event = controller.step(action="GetMapViewCameraProperties")
-    ## third camera's fov
-    third_fov = 60
-
-    if not birds_eye_view:
-        ## top_view(slope)
-        pose = initialize_side_camera_pose(event.metadata["sceneBounds"], event.metadata["actionReturn"], third_fov,
-                                           slope_degree, down_angle)
-    else:
-        ## BEV
-        pose = event.metadata["actionReturn"]
-        is_rotate = False  ## assume that BEV do not need rotation
-
-    event = controller.step(
-        action="AddThirdPartyCamera",
-        skyboxColor="black",
-        fieldOfView=third_fov,
-        **pose
-    )
-
-    ## collect frame
-    first_view_frames = []
-    third_view_frames = []
-
-    ## use keyboard control agent
-    keyboard_play(controller, third_view_frames, first_view_frames, is_rotate, rotate_per_frame)
-
-    if generate_video:
-        save_videos(scene_name, first_view_frames, third_view_frames)
-
-    if generate_gif:
-        save_gifs(scene_name, first_view_frames, third_view_frames)
+        if generate_gif:
+            save_gifs(scene_name, first_view_frames, third_view_frames)
 
 
 if __name__ == "__main__":
-    main(scene_name="FloorPlan19_physics",  ## room
+    player = KeyboardPlayer()
+    player.run(scene_name="FloorPlan19_physics",  ## room
          gridSize=0.25, rotateStepDegrees=15,  ## agent step len and rotate degree
          birds_eye_view=False,  ## Bird's-eye view or top view(slope)
          slope_degree=60,  ## top view(slope)'s initial rotate degree
