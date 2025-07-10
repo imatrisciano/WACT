ff388208ce7ef0a05c89f324bb4ea1ec15dd0592
Ivano Matrisciano <ivanomatrisciano@gmail.com>
2025-07-10 18:08:53 +0200

    Implemented PredictorPipeline

---
+++ src/Predictors/PredictorPipeline.py
@@ -0,0 +1,98 @@
+import os
+
+import numpy as np
+
+from src.DataPreprocessing.ObjectEncoder import ObjectEncoder
+from src.DataPreprocessing.WorldStatusEncoder import MostChangesWorldStatusEncoder
+from src.ObjectStore.MetadataObjectStore import MetadataObjectStore
+from src.Predictors.ClassifierManager import ClassifierManager
+from src.Predictors.MyDataset import MyDataset
+
+
+class PredictorPipeline:
+    """
+    Implements an end to end predictor pipeline for Which Action Caused This
+    """
+
+    def __init__(self):
+        NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS = 10
+
+        self.object_store = MetadataObjectStore("../../ai2thor-hugo/objects/")
+        self.whole_dataset: MyDataset = MyDataset(self.object_store,
+                                             use_cache=True,
+                                             cache_location="../data/dataset_cache/",
+                                             number_of_significant_objects=NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS)
+        self.classifier_manager = ClassifierManager(
+            object_store=self.object_store,
+            dataset=self.whole_dataset,
+            number_of_significant_objects=NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS,
+            model_save_path="predictor.pth",
+            # device="cuda"
+        )
+        # classifier_manager.train()
+
+        object_encoder = ObjectEncoder()
+        self.world_status_encoder = MostChangesWorldStatusEncoder(object_encoder,
+                                                             number_of_significant_objects=NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS)
+
+    def train(self):
+        self.classifier_manager.train()
+
+    def predict(self, action_data: dict) -> (str, str):
+        """
+        Performs action and action target prediction given the action data
+        :param action_data: A dictionary containing 'before_world_status', 'after_world_status', 'action_name', 'action_objective_id' and 'liquid' keys
+        :return: The action class name and the action target object id
+        """
+
+        # Prepare the world encoding
+        world_encoding = self.world_status_encoder.encode_action_data(action_data)
+        world_encoding = np.array(world_encoding, dtype=np.float32)
+
+        # Prepare network input data
+        network_input_shape = (1, self.world_status_encoder.total_objects_in_world_encoding,
+                               self.world_status_encoder.object_encoder.object_encoding_size)  # batch size, how many objects per network input, object encoding length
+        world_encoding = np.reshape(world_encoding, network_input_shape)
+
+        # Run inference
+        action_class, action_class_name, object_index = self.classifier_manager.inference(world_encoding)
+
+        # Retrieve the action target encoding from the world encoding and the object index inferred by the network
+        action_target_encoding = world_encoding[0, object_index, :]  # batch, object index in the world encoding, the entirety of the object encoding
+        decoded_object = self.world_status_encoder.object_encoder.decode(action_target_encoding)  # decode the object
+
+        decoded_object_id = decoded_object["objectId"]
+        return action_class_name, decoded_object_id
+
+    def predict_from_file(self, file_path):
+        """
+        Loads an action file from disk and performs prediction on it
+        :param file_path: The action data file
+        :return: detected_action_name: str, detected_object_id: str, true_action_name: str, true_object_id: str
+        """
+
+        action_data = self.object_store.load(file_path)  # load the file from disk
+
+        detected_action_name, detected_object_id = self.predict(action_data)
+
+        true_action_name = action_data["action_name"]
+        true_object_id = action_data["action_objective_id"]
+
+        return detected_action_name, detected_object_id, true_action_name, true_object_id
+
+    def predict_from_before_and_after_world_status(self, before_world_status: dict, after_world_status: dict):
+        action_data = {
+            "before_world_status": {
+                "objects": before_world_status['objects']
+            },
+            "after_world_status": {
+                "objects": after_world_status['objects']
+            },
+            "action_name": "",
+            "action_objective_id": "",
+            "liquid": ""
+        }
+        return self.predict(action_data)
+
+    def list_training_files(self) -> list[os.PathLike | str]:
+        return self.object_store.list_files()
--- src/main.py
+++ src/main.py
@@ -1,76 +1,14 @@
-import numpy as np
 from tqdm import tqdm
+from src.Predictors.PredictorPipeline import PredictorPipeline
 
-from ObjectStore.MetadataObjectStore import MetadataObjectStore
-from DataPreprocessing.ChangeDetector import ChangeDetector
-from DataAnalysis.DatasetAnalysis import DatasetAnalyzer
-from DataPreprocessing.WorldStatusEncoder import MostChangesWorldStatusEncoder
-from DataPreprocessing.ObjectEncoder import ObjectEncoder
-from src.Predictors.ClassifierManager import ClassifierManager
-from src.Predictors.MyDataset import MyDataset
 
-NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS = 10
-object_store = MetadataObjectStore("../../ai2thor-hugo/objects/")
-whole_dataset: MyDataset = MyDataset(object_store,
-      use_cache=True,
-      cache_location="../data/dataset_cache/",
-      number_of_significant_objects=NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS)
-classifier_manager = ClassifierManager(
-    object_store=object_store,
-    dataset=whole_dataset,
-    number_of_significant_objects=NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS,
-    model_save_path = "predictor.pth",
-    # device="cuda"
-)
-#classifier_manager.train()
+predictor_pipeline = PredictorPipeline()
 
-object_encoder = ObjectEncoder()
-world_status_encoder = MostChangesWorldStatusEncoder(object_encoder, number_of_significant_objects=NUMBER_OF_ACTION_SIGNIFICANT_OBJECTS)
+for file_path in tqdm(predictor_pipeline.list_training_files()):
+    detected_action_name, detected_object_id, true_action_name, true_object_id = predictor_pipeline.predict_from_file(file_path)
 
-for file_path in tqdm(object_store.list_files()):
-    action_data = object_store.load(file_path) # load the file from disk
-
-    # Prepare the world encoding
-    world_encoding = world_status_encoder.encode_action_data(action_data)
-    world_encoding = np.array(world_encoding, dtype=np.float32)
-
-    # Prepare network input data
-    network_input_shape = (1, world_status_encoder.total_objects_in_world_encoding, world_status_encoder.object_encoder.object_encoding_size) # batch size, how many objects per network input, object encoding length
-    world_encoding = np.reshape(world_encoding, network_input_shape)
-
-    # Run inference
-    action_class, action_class_name, object_index = classifier_manager.inference(world_encoding)
-
-    # Retrieve the action target encoding from the world encoding and the object index inferred by the network
-    action_target_encoding = world_encoding[0, object_index, :] # batch, object index in the world encoding, the entirety of the object encoding
-    decoded_object = world_status_encoder.object_encoder.decode(list(action_target_encoding)) # decode the object
-
-    true_action_name = action_data["action_name"]
-    true_object_id = action_data["action_objective_id"]
-    decoded_object_id = decoded_object["objectId"]
-
-    dataset = classifier_manager.whole_dataset
-
-    print(f" [Truth]:      Action: {dataset.label2id(true_action_name)} ({true_action_name}) on object - ({true_object_id})")
-    print(f" [Prediction]: Action: {action_class} ({action_class_name}) on object {object_index} ({decoded_object_id})")
+    print(f" [Truth]:      Action: {true_action_name} on object {true_object_id}")
+    print(f" [Prediction]: Action: {detected_action_name} on object {detected_object_id}")
     print("")
 
 exit(0)
-
-
-#object_store = MetadataObjectStore("../../ai2thor-hugo/objects/")
-#change_detector = ChangeDetector()
-#
-#object_encoder = ObjectEncoder()
-#world_status_encoder = MostChangesWorldStatusEncoder(object_encoder, number_of_significant_objects=10)
-#
-#dataset_analyzer = DatasetAnalyzer(object_store, change_detector)
-#dataset_analyzer.plot_dataset_info()
-#
-#
-#for file_path in tqdm(object_store.list_files()):
-#    obj = object_store.load(file_path)
-#    world_encoding = world_status_encoder.encode_action_data(obj)
-#
-#    print(world_encoding)
-
