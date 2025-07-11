476cd8d9d7a54440b6b15a847bd67d0c5c6cb828
ivano <ivanomatrisciano@gmail.com>
2025-07-11 11:01:13 +0200

    No overloads in python!!11!

--- src/Models/PropertyChange.py
+++ src/Models/PropertyChange.py
@@ -1,16 +1,18 @@
-class PropertyChange:
-    def __init__(self):
-        self.property_path: list[str] = None
-        self.before_value = None
-        self.after_value = None
+from typing import Optional
 
-    def __init__(self, property_path: list[str], before_value, after_value):
-        self.property_path = property_path
-        self.before_value = before_value
-        self.after_value = after_value
+
+class PropertyChange:
+    def __init__(self,
+                 property_path: Optional[list[str]] = None,
+                 before_value: Optional = None,
+                 after_value: Optional = None):
+
+        self.property_path: Optional[list[str]] = property_path
+        self.before_value: Optional = before_value
+        self.after_value: Optional = after_value
 
     def property_path_str(self) -> str:
-        return str.join('.', self.property_path)
+        return str.join('.', self.property_path) if self.property_path is not None else ""
 
     def __str__(self):
         return f"{self.property_path_str()}: '{self.before_value}' -> '{self.after_value}'"
